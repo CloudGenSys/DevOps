2d1ea47292dba6242893213f21ac9ff38c00dc70
LakshmanShyam <shyam.katta@cloudgensys.com>
2017-09-12 14:55:48 +0000

    updated script

--- CGDemo/cate.sh
+++ CGDemo/cate.sh
@@ -1,5 +1,5 @@
-JMETERDIR=/home/cgadmin/catemeter/catemeter-3.0
-HOSTNAME=$1
-PORT=$2
-JMXSOURCE=$3
-sh $JMETERDIR/bin/catemeter.sh -Jhostname=$HOSTNAME -Jport=$PORT -n -t $JMXSOURCE
+#JMETERDIR=/home/cgadmin/catemeter/catemeter-3.0
+#HOSTNAME=$1
+#PORT=$2
+#JMXSOURCE=$3
+sh /home/cgadmin/catemeter/catemeter-3.0/bin/catemeter.sh -Jhostname=$1 -Jport=$2 -n -t $3
